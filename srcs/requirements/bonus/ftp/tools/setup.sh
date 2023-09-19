bb3ab9d2373fa92c68981feac900e6e204bca6aa
rida <rakhsas@rakhsas.42.fr>
2023-09-19 07:31:46 -0500

    begin ftp setup

---
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "FTP_SERVER INSTALLATION BEGIN!!!"
+
+echo "pasv_address=$DOMAIN_NAME" >> /etc/vsftpd.conf
+
+useradd $FTP_USER && echo "$FTP_USER:$FTP_USER_PASS" | chpasswd
+mkdir -p /home/$FTP_USER
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+chmod 755 /home/$FTP_USER
+
+echo "USER $FTP_USER CREATED WITH PASSWORD $FTP_USER_PASS";
+
+
+vsftpd /etc/vsftpd.conf
